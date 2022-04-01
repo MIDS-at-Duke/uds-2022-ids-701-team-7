3203f42414c1aaa7d529f013d4f349497e04a8fd
Athena Liu <43796329+Athena112233@users.noreply.github.com>
2022-04-01 17:46:28 -0400

    updated MergeData.py

--- 10_code/MergeData.py
+++ 10_code/MergeData.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 
 def merge_data(path):
     # load data files
-    weather = pd.read_csv('weather.csv').drop(columns = 'Unnamed: 0')
-    RPS = pd.read_csv('StatesRPS.csv').drop(columns = ['Percentage', 'TargetYear'])
-    coal = pd.read_csv('20_intermediate_files/clean_coal.csv').drop(columns = 'Unnamed: 0')
-    gas = pd.read_csv('20_intermediate_files/clean_gas.csv').drop(columns = 'Unnamed: 0')
-    petroleum = pd.read_csv('00_source_data/petroleum price.csv')
-    solar = pd.read_csv('00_source_data/solar-pv-prices.csv')
-    generation = pd.read_csv('clean_energy_percent_processed.csv').drop(columns = 'Unnamed: 0')
+    weather = pd.read_csv('../00_source_data/weather.csv').drop(columns = 'Unnamed: 0')
+    RPS = pd.read_csv('../00_source_data/StatesRPS.csv').drop(columns = ['Percentage', 'TargetYear'])
+    coal = pd.read_csv('../00_source_data/clean_coal.csv').drop(columns = 'Unnamed: 0')
+    gas = pd.read_csv('../00_source_data/clean_gas.csv').drop(columns = 'Unnamed: 0')
+    petroleum = pd.read_csv('../00_source_data/petroleum price.csv')
+    solar = pd.read_csv('../00_source_data/solar-pv-prices.csv')
+    generation = pd.read_csv('../00_source_data/clean_energy_percent_processed.csv').drop(columns = 'Unnamed: 0')
 
     # clean data again before merging
     # set temporal range to 1990 - present
@@ -19,18 +19,13 @@ def merge_data(path):
     solar = solar.loc[solar['Year'] >= 1990]
 
     # change column names
-    coal = coal.rename(columns = {'year': 'Yer', 'WPS051': 'CoalPrice'})
+    coal = coal.rename(columns = {'year': 'Year', 'WPS051': 'CoalPrice'})
     solar = solar.drop(columns = ['Entity', 'Code']).rename(columns = {'Solar PV Module Cost (2019 US$ per W)': 'SolarPrice'})
-    petroleum = petroleum.rename(columns = {'U.S. Crude Oil First Purchase Price (Dollars per Barrel)':'PetroleumPrice'})
+    petroleum = petroleum.rename(columns = {'Date': 'Year', 'U.S. Crude Oil First Purchase Price (Dollars per Barrel)':'PetroleumPrice'})
     gas = gas.rename(columns = {'year': 'Year', '\xa0value': 'GasPrice'})
     generation = generation.rename(columns = {'YEAR':'Year', 'STATE': 'Abbreviation', 
                                           'clean energy generation': 'CleanEnergyGeneration', 
                                           'clean_energy_percent': 'CleanEnergyPercent'})
-
-    # merge energy price into a single data frame
-    energy_df = coal.merge(solar, right_on = 'Year', left_on = 'Year', how = 'left')
-    energy_df = energy_df.merge(petroleum, right_on = 'Year', left_on = 'Year', how = 'left')
-    energy_df = energy_df.merge(gas, right_on = 'Year', left_on = 'Year', how = 'left')
 
     # merge energy price into a single data frame
     energy_df = coal.merge(solar, right_on = 'Year', left_on = 'Year', how = 'left')
